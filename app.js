5cb5cee3a1af266ac5568a1a4d42d8c17d295a9e
sreshma-006 <60685795+sreshma-006@users.noreply.github.com>
2022-03-15 13:04:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+const getdata = async (country) => {
+    try {
+        const apikey = '5e752ff26225439a823eafc2e1ce15c7'
+        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apikey}`)
+
+        if (data.status === 200)
+            return await data.json();
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+getdata("in")
+const rendernews = (list) => {
+
+
+    let news = ``
+    list.map((ele) =>
+        news +=
+        `<div class="news">
+ <img src="${ele.urlToImage}" class="newsposter" alt=${ele.title}>
+ <h2 class="newsheader">${ele.title}</h2>
+ <p class="newsdescription">${ele.description}</p>
+ <a href=${ele.url} >Read More</a>
+</div>\n`
+    )
+
+    return news
+}
+
+const render = async (country) => {
+    document.querySelector(".newslist").innerHTML = ""
+    let data = await getdata(country)
+    document.querySelector(".newslist").innerHTML = rendernews(data.articles)
+}
+const indannews = () => {
+    document.title = "India News"
+    render("in")
+    return false
+
+}
+
+const usnews = () => {
+    document.title = "US News"
+
+    render("us")
+    return false
+
+}
+
+const homepage = () => {
+    indannews()
+
+}
+homepage()
+
+
+
+
+
+
+
+
+
+const displaydropdown = () => {
+    document.querySelector(".dropdown").classList.toggle("flex")
+
+}
+document.querySelector(".countrylist").addEventListener("click", displaydropdown)
+document.querySelector(".dropdown").addEventListener("mouseout", displaydropdown)
